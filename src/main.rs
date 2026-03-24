902ba6509e982fda55e090243f92ee2ada5755c2
Salvo <salvo.rapisarda@sainsburys.co.uk>
2026-03-24 18:31:23 +0000

    feat: Falling enemy squares

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,80 @@
 use macroquad::prelude::*;
 
-// struct Shape {
-//     size: f32,
-//     speed: f32,
-//     x: f32,
-//     y: f32,
-// }
+struct Shape {
+    size: f32,
+    speed: f32,
+    x: f32,
+    y: f32,
+}
 
 #[macroquad::main("Flyway")]
 async fn main() {
-    let mut x = screen_width() / 2.0;
-    let mut y = screen_height() / 2.0;
     const MOVEMENT_SPEED: f32 = 200.0;
-    x = clamp(x, 0.0, screen_width());
-    y = clamp(y, 0.0, screen_height());
+
+    // let mut x = screen_width() / 2.0;
+    // let mut y = screen_height() / 2.0;
+
+    let mut squares:Vec<Shape> = vec![];
+
+    let mut circle = Shape {
+        size: 32.0,
+        speed: MOVEMENT_SPEED,
+        x: screen_width() / 2.0,
+        y: screen_height() / 2.0,
+    };
+
+    circle.x = clamp(circle.x, 0.0, screen_width());
+    circle.y = clamp(circle.y, 0.0, screen_height());
     loop {
         clear_background(DARKPURPLE);
         let delta_time = get_frame_time();
 
         if is_key_down(KeyCode::Right) {
-            x += MOVEMENT_SPEED * delta_time;
+            circle.x += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            x -= MOVEMENT_SPEED * delta_time;
+            circle.x -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            y += MOVEMENT_SPEED * delta_time;
+            circle.y += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Up) {
-            y -= MOVEMENT_SPEED * delta_time;
+            circle.y -= MOVEMENT_SPEED * delta_time;
         }
 
-        x = clamp(x, 0.0, screen_width());
-        y = clamp(y, 0.0, screen_height());
-        draw_circle(x, y, 16.0, YELLOW);
+        circle.x = clamp(circle.x, 0.0, screen_width());
+        circle.y = clamp(circle.y, 0.0, screen_height());
+
+        // Add enemy squares
+        if rand::gen_range(0, 99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+                size,
+                speed: rand::gen_range(50.0, 150.0),
+                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                y: -size,
+            });
+        }
+
+        // Update square positions
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
+
+        // Remove invisible squares
+        squares.retain(|square| square.y < screen_height() + square.size);
+
+        draw_circle(circle.x, circle.y, 16.0, YELLOW);
+
+        for square in &squares {
+            draw_rectangle(
+                square.x - square.size / 2.0,
+                square.y - square.size / 2.0,
+                square.size,
+                square.size,
+                GREEN,
+            );
+        }
 
         next_frame().await
     }
